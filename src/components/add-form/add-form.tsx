af67c3218e7bd5debd0669e8bdcb3cb206d7e8ce
khaled <sky455495@gmail.com>
2023-06-08 14:41:22 +0300

    adding the add form component

---
+++ src/components/add-form/add-form.tsx
@@ -0,0 +1,10 @@
+import "./add-form.css";
+function AddForm() {
+	return (
+		<form className="AddForm">
+			<input type="text" id="text" name="text" />
+		</form>
+	);
+}
+
+export default AddForm;

2c5c5da6950c35ff7840e93e884437e042042b35
khaled <sky455495@gmail.com>
2023-06-09 17:38:36 +0300

    adding the delet-icon

--- src/components/task-list/task-list.tsx
+++ src/components/task-list/task-list.tsx
@@ -2,8 +2,29 @@ import "./task-list.css";
 export function TaskList() {
 	return (
 		<ul className="task-list">
-			<li className="task">{"hi fdjas;ljfa;s".repeat(456)}</li>
-			<li className="task">hi</li>
+			<div className="list">
+				<li className="task">{"hi fdjas;ljfa;s".repeat(456)}</li>
+			</div>
+			<div className="list">
+				<li className="task">{"hi fdjas;ljfa;s".repeat(456)}</li>
+			</div>
+			<div className="list">
+				<li className="task">{"hi fdjas;ljfa;s".repeat(456)}</li>
+			</div>
+			<div className="list">
+				<li className="task">{"hi fdjas;ljfa;s".repeat(456)}</li>
+			</div>
+			<div className="list">
+				<li className="task">{"hi fdjas;ljfa;s".repeat(456)}</li>
+			</div>
+			<div className="list">
+				<span className="delete-icon-disable">x</span>
+				<li className="task task-focus">
+					hi fldasjk; jkldaf;s kladsf;j fdsl;alk dsfja fsal jasl
+					jdsfdsjlak fas fasfsdafsdafds fsdafdsfsdfds fjdsaljflasjfj
+					fjsakjflaj
+				</li>
+			</div>
 			<li className="task">hi</li>
 		</ul>
 	);

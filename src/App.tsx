19ee5f006c12de208cd5bead62e6458031627616
khaled <sky455495@gmail.com>
2023-06-09 16:34:24 +0300

    adding the task list style

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import AddForm from "./components/add-form/add-form";
+import { TaskList } from "./components/task-list/task-list";
 
 function App() {
 	return (
 		<div className="App">
 			<div className="App-container">
 				<AddForm />
+				<TaskList />
 			</div>
 		</div>
 	);
---
+++ src/components/task-list/task-list.tsx
@@ -0,0 +1,10 @@
+import "./task-list.css";
+export function TaskList() {
+	return (
+		<ul className="task-list">
+			<li className="task">{"hi fdjas;ljfa;s".repeat(456)}</li>
+			<li className="task">hi</li>
+			<li className="task">hi</li>
+		</ul>
+	);
+}
